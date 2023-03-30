b7e914483c99d7d320337ed5b873f7f175cbec83
Jean-Bernard Massée <2t1xl6wlp5g4-student@anonymous-codio.com>
2023-03-30 15:51:16 +0000

    Etape 5 : versions ok de processExitingVehicleTest, testProcessIncomingVehicle et processExitingVehicleTestUnableUpdate avant validation mentor

--- parkingsystem/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -31,7 +31,7 @@ public class ParkingServiceTest {
     private static ParkingService parkingService;
 
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
+    
     @Mock
     private static InputReaderUtil inputReaderUtil;
 
@@ -49,18 +49,8 @@ public class ParkingServiceTest {
         try {
           
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");  // pour appel de methode getVehicleRegNumber ex : processIncomingVehicle
-/*
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false); 
-            Ticket ticket = new Ticket();
-            ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));  //tests effectués pour une durée de stationnement de 1 heure
-            ticket.setParkingSpot(parkingSpot); // passage dans objet ticket du parkingSpot crée ligne 41
-            ticket.setVehicleRegNumber("ABCDEF");
 
-            when(ticketDAO.getTicket(anyString())).thenReturn(ticket);  // si appel de BDD par methode getTicket, retourne objet ticket crée dans le test
-*/
-
-            //when(ticketDAO.getNbTicket(anyString())).thenReturn(3); //test pour un client recurrent
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);  // pareil avec parkingSpot
+            //when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);  // pareil avec parkingSpot
 
             parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
 
@@ -71,33 +61,27 @@ public class ParkingServiceTest {
             throw  new RuntimeException("Failed to set up test mock objects");
         }
     }
-  /*  
-    @AfterEach
-        private void tearDown() {
-        System.setOut(standardOut);
-    }
-*/
+
     @Test
     public void testProcessIncomingVehicle(){
- // 2 conditions : (parkingSpot !=null && parkingSpot.getId() > 0)
-
-        when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(4);  // 2 conditions : (parkingSpot !=null && parkingSpot.getId() > 0)
+        when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(4);  
         when(inputReaderUtil.readSelection()).thenReturn(1); 
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);  
+        when(ticketDAO.getNbTicket(anyString())).thenReturn(4);  // vehicule recurrent
        
-        String expectedMessage = ("Heureux de vous revoir ! En tant qu’utilisateur régulier de notre parking, vous allez obtenir une remise de 5%");
-
         //WHEN
         parkingService.processIncomingVehicle();
 
         //THEN
-        
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class)); //  test d'un appel de methode
         verify(ticketDAO, Mockito.times(1)).saveTicket(any(Ticket.class));
         verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString());
+        assertTrue(outputStreamCaptor.toString().contains("Heureux de vous revoir ! En tant qu’utilisateur régulier de notre parking, vous allez obtenir une remise de 5%")); //  test discount vehicule recurrent
 
-        //assertEquals(expectedMessage, outputStreamCaptor.toString());
-        
 /*
+        String expectedMessage = ("Heureux de vous revoir ! En tant qu’utilisateur régulier de notre parking, vous allez obtenir une remise de 5%");
+        //assertEquals(expectedMessage, outputStreamCaptor.toString());
+
         verify(ticketDAO).saveTicket(ticketCaptor.capture()); // test de valeur d'un parametre passé à une methode
         Ticket savedTicket = new Ticket();
         savedTicket = ticketCaptor.getValue();
@@ -108,50 +92,48 @@ public class ParkingServiceTest {
     @Test
     public void processExitingVehicleTest(){    // ne retourne rien donc on teste des appels de methodes
 
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false); 
+        Ticket ticket = new Ticket();
+        ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));  //tests effectués pour une durée de stationnement de 1 heure
+        ticket.setParkingSpot(parkingSpot); // passage dans objet ticket du parkingSpot crée ligne 41
+        ticket.setVehicleRegNumber("ABCDEF");
 
-
-            //when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");  // pour appel de methode getVehicleRegNumber ex : processIncomingVehicle
-
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false); 
-            Ticket ticket = new Ticket();
-            ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));  //tests effectués pour une durée de stationnement de 1 heure
-            ticket.setParkingSpot(parkingSpot); // passage dans objet ticket du parkingSpot crée ligne 41
-            ticket.setVehicleRegNumber("ABCDEF");
-
-            when(ticketDAO.getTicket(anyString())).thenReturn(ticket);  // si appel de BDD par methode getTicket, retourne objet ticket crée dans le test
-
-
-
-
-
-
-
-
-
+        when(ticketDAO.getTicket(anyString())).thenReturn(ticket);  // si appel de BDD par methode getTicket, retourne objet ticket crée dans le test
         when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true); 
+
         // WHEN
         parkingService.processExitingVehicle();
 
         // THEN
         verify(ticketDAO, Mockito.times(1)).getTicket(anyString());
-        verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString()); //verifie qu'on regarde bien si un vehicule est recurrent ou pas
+        verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString()); 
         verify(ticketDAO, Mockito.times(1)).updateTicket(any(Ticket.class));
         verify(parkingSpotDAO, times(1)).updateParking(any(ParkingSpot.class)); // verifie que la methode updateParking est appelée une seule fois 
     }
-/*
+
     @Test
-    public void processExitingVehicleTestUnableUpdate(){    // resultat : doit verifier affichage du message "Unable to update ticket information. Error occurred"
-        when(ticketDAO.updateTicket(any(Ticket.class)).thenReturn(false);
-        // GIVEN
-        String expectedMessage = ("Unable to update ticket information. Error occurred") ;
+    public void processExitingVehicleTestUnableUpdate(){    
+      /* Soit on teste affichage du message "Unable to update ticket information. Error occurred" - code dans else
+        Soit on teste que parkingSpotDAO.updateParking(parkingSpot); n'est pas appelé - par defaut
+      */
+        
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false); 
+        Ticket ticket = new Ticket();
+        ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));  //tests effectués pour une durée de stationnement de 1 heure
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+
+        when(ticketDAO.getTicket(anyString())).thenReturn(ticket);  // si appel de BDD par methode getTicket, retourne objet ticket crée dans le test
+        when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(false);
    
         // WHEN
         parkingService.processExitingVehicle();
 
         // THEN
-        assertEquals(expectedMessage, outputStreamCaptor.toString());
+        verify(parkingSpotDAO, times(0)).updateParking(any(ParkingSpot.class)); 
+        assertTrue(outputStreamCaptor.toString().contains("Unable to update ticket information. Error occurred"));
+
     }
-*/
 }
         
 
